661b3e6f2d493d57aef510fd10cc842f8bcc4d69
skerschb <sue.kerschbaumer@10gen.com>
2018-03-09 14:26:35 -0500

    uriwriter

--- sphinxext/uriwriter.py
+++ sphinxext/uriwriter.py
@@ -45,14 +45,17 @@ URIWRITER_TEMPLATE = fett.Template('''
            var replacementTarget = document.getElementsByTagName("pre");
            for(var i = 0 ; i < replacementTarget.length ; i++){
                var item = replacementTarget.item(i);
+ 
+               if (item.innerHTML.indexOf(current)>-1) {
                
-               item.innerHTML = item.innerHTML.replace("&lt;URISTRING&gt;", uri).replace("{host}", obj['hostname']).
+                   item.innerHTML = item.innerHTML.replace("&lt;URISTRING&gt;", uri).replace("{host}", obj['hostname']).
                                     replace("{port}", obj['port']).
                                     replace("{db}", obj['db']).
                                     replace("{authdb}", obj['authdb']).
                                     replace("{username}", obj['username']);
-               current = item.innerHTML;
-               console.log(item.innerHTML);
+                   current = item.innerHTML;
+                   console.log(item.innerHTML);
+               }
            }  
        }   
            
